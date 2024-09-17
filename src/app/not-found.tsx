1fb8c3396648a2018b3055928476dd84a72c43ae
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-09-17 23:07:30 +0800

    Not Found Page and File Colocation

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound () {
+    return (
+        <div>
+            <h1>Page not found</h1>
+            <p>Counld not find requested resource</p>
+        </div>
+    )
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,18 +1,16 @@
-/**
- * This function takes in parameters for a product ID and review ID and displays a heading with the
- * corresponding values.
- * @param  - The ReviewDetail function takes a single object as its argument, which has a nested
- * `params` object. The `params` object contains two properties: `productId` of type string and
- * `reviewId` of type string.
- * @returns A React functional component named ReviewDetail is being returned. This component takes a
- * destructured object as a parameter with a nested params object containing productId and reviewId
- * properties. The component renders an h1 element displaying the reviewId and productId values.
- */
+import { notFound } from "next/navigation"
+
 export default function ReviewDetail ({ params } : {
      params: {
          productId: string 
          reviewId: string
         } 
-    }) {
-    return <h1> Review {params.reviewId} for product {params.productId}</h1>
+    })
+     {
+    if(parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+    return (
+        <h1> Review {params.reviewId} for product {params.productId}</h1>
+    )
 }
