85d59238fe1b0b82ca0a08b984e4880b382fb123
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-09-21 23:30:35 +0800

    Adding dynamic routing metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header
+        style={{
+          backgroundColor: "#FFD04C",
+          padding: "1rem"
+        }}
+        >
+          <h1>Header</h1>
+        </header>
+        {children}
+        <footer
+        style={{
+          backgroundColor: "#FE6132",
+          padding: "1rem"
+        }}
+        >
+          <h1>Footer</h1>
+        </footer>
+      </body>
     </html>
   )
 }
