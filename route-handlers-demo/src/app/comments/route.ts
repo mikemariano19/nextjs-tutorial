d3c26804e0353e5268b2afab48a724510aa64e9f
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-10-20 23:26:52 +0800

    Handling get and post

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,5 +1,22 @@
 import { comments } from "./data"
 
+// GET
 export async function GET() {
     return Response.json(comments)
+}
+
+// POST
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newcomment = {
+        id: comments.length + 1,
+        text: comment.text
+    }  
+    comments.push(newcomment)
+    return new Response(JSON.stringify(newcomment), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201,
+    })
 }
