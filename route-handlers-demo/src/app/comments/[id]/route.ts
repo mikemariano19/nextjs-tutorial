d747c4bd1a559848a0855c7fbf2642d46b60240c
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-10-21 22:47:38 +0800

    dynamic route handling

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,5 +1,12 @@
 import { comments } from "../data"
 
-export async function GET() {
-    return new Response("GET handler")
+export async function GET(
+    _request: Request, 
+    { params } : { params: { id:string } }
+) {
+    const comment = comments.find(
+        (comment) => comment.id === parseInt(params.id)
+    )
+
+    return  Response.json(comment)
 }
