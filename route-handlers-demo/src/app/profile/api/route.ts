12993c67feb262767f230d9736dea53b532e17d2
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-10-27 22:24:29 +0800

    Setting Cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,16 +1,22 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const requestHeaders= new Headers(request.headers)
     const headersList = headers()
 
+    cookies().set("resultsPerPage", "20")
+    const theme = request.cookies.get("theme")
+
     console.log(requestHeaders.get("Authorization"))
     console.log(headersList.get("Authorization"))
+    console.log(theme)
+    console.log(cookies().get("resultsPerPage"))
 
     return new Response("<h1>Profile API data</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark",
         }
     })
 }
