b8f919a73c09056a01750de5fd28f2270a33a0a1
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-11-18 23:20:24 +0800

    Third party packages

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,15 +1,43 @@
 "use client"
 
+import React from "react";
 import { serverSideFunction } from "@/app/utils/server-utils";
 
 
 export default function ClientRoutePage() {
     console.log("Client route rendered");
     const result = serverSideFunction();
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
     return (
         <>
             <h1>ClientRoutePage</h1>
             <p>{result}</p>
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </Slider>
         </>
     )
 }
