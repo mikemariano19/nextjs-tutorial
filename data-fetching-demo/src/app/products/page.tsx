e8f7debfc9aa2a82c281de1cb12f7ae9175eba39
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-12-05 22:58:17 +0800

    Opting out of caching

--- data-fetching-demo/src/app/products/page.tsx
+++ data-fetching-demo/src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers";
+
 type Product = {
     id: number,
     title: string,
@@ -8,6 +10,12 @@ type Product = {
 export default async function ProductsPage() {
     const response = await fetch('http://localhost:3001/products')
     const products = await response.json();
+
+    const cookieStore = cookies();
+    (await cookieStore).get("theme")
+
+    const detailsResponse = await fetch('http://localhost:3001/products/1')
+    const details = await detailsResponse.json();
     return (
         <ul className="space-y-4 p-4">
           {products.map((product: Product) => (
@@ -18,6 +26,7 @@ export default async function ProductsPage() {
               <h2 className="text-xl font-semibold">{product.title}</h2>
               <p>{product.description}</p>
               <p className="text-lg font-medium">${product.price}</p>
+              <p>{details.title}</p>
             </li>
           ))}
         </ul>
